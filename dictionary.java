c2f7b120d8728d6d401123998b4bc30ef1374070
Konsklik <94300188+Konsklik@users.noreply.github.com>
2022-03-08 11:41:56 +0200

    dictionary generation
    dictionary constructor for both download and saved files implemented

--- dictionary.java
+++ dictionary.java
@@ -1,35 +1,133 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonString;
+
+
+
+class InvalidCountException extends Exception{}
+class UndersizeException extends Exception{}
+class InvalidRangeException extends Exception{
+	public InvalidRangeException(){
+		super("word under 6 letters");
+	}
+}
+class UnbalncedException extends Exception{
+	public UnbalncedException(String errorMessage){
+		super(errorMessage);
+	}
+}
+
 
 public class dictionary {
-    String[] words = {};
-    int[][] propabilities = {};
-    public dictionary(String dic_ID){
-        File dic = new File(dic_ID);
-        if(!f.isFile()){
-            url_handler(f, "example.com/" + dic_ID);
+    Set<String>  words = new HashSet<String>();
+    public dictionary(String dic_ID) throws InvalidCountException, UndersizeException, UnbalncedException, InvalidRangeException{
+        String directory = new File("").getAbsolutePath();
+        File dic = new File(directory + "\\multimedia\\hangman_" + dic_ID + ".txt");
+        if(!dic.isFile()){
+            url_handler(dic, "https://openlibrary.org/works/" + dic_ID + ".json");
         }
-        Scanner reader = new Scanner(f);
-        int i = 0
-        while (reader.hasNextLine()){
-            words[i] = reader.nextLine();
-            i++;
+        else{
+            file_handler(dic);
         }
     };
-    public String get_word(){
-        return Random(words);
+    
+    private void url_handler(File dic, String url){
+        try {
+            InputStream is = new URL(url).openStream();
+            JsonReader rdr = Json.createReader(is);
+            JsonObject obj = rdr.readObject();
+            JsonString jtext = get_desc_value(obj);
+            String text = jtext.getString();
+            write_dic(dic, text);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     };
-    public void calc_propabilities(){
-
+    
+    private  JsonString get_desc_value(JsonObject obj){
+        JsonString result;
+        try{
+            JsonObject step = obj.getJsonObject("description");
+            result = step.getJsonString("value");
+        }catch(ClassCastException e){
+            result = obj.getJsonString("description");
+        }
+        return result;
     };
 
-    private void url_handler(File f, String url){
-        //download
-        tokenize(data)
+    private  void write_dic(File dic, String data) throws IOException{
+        words = tokenize(data);
+        dic.getParentFile().mkdirs();
+        dic.createNewFile();
+        FileWriter wr = new FileWriter(dic);
+        for (String word : words){
+            wr.write(word + '\n');
+        }
+        wr.close();
+    }
+
+	private  Set<String> tokenize(String data){
+        String[] words_arr = data.split("[^a-zA-Z]+",0);
+		Set<String> result = new HashSet<String>();
+        for (int i = 0; i < words_arr.length; i++) {
+            if (words_arr[i].length() >= 6){
+                String to_add = words_arr[i].toUpperCase();
+				result.add(to_add);
+			}
+        }
+		if (result.size() >= 20){
+	        return result;
+		}
+		return null;
     };
 
-    private String[] tokenize(String data){
-
-    };
+    private void file_handler(File dic) throws InvalidCountException, UndersizeException, UnbalncedException, InvalidRangeException {
+        try {
+            BufferedReader rd = new BufferedReader(new FileReader(dic));
+            String line = rd.readLine();
+            int line_num = 0, over_9 = 0;
+            while (line != null) {
+                if (words.contains(line)) {
+                    rd.close();
+                    throw new InvalidCountException();
+                }
+                if (line.length() < 6) {
+                    rd.close();
+                    throw new InvalidRangeException();
+                }
+                words.add(line);
+                if (line.length() >= 9) {
+                    over_9++;
+                }
+                line_num++;
+                line = rd.readLine();
+            }
+            if (over_9 < (int) (line_num * 0.2)) {
+                rd.close();
+                throw new UnbalncedException("minimum number of words with length greater than 9: "
+                        + (int) (line_num * 0.2) + "\n number of words with length greater than 9: " + over_9);
+            }
+            if (line_num < 20) {
+                rd.close();
+                throw new UndersizeException();
+            }
+            rd.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- start.java
+++ start.java
@@ -1,23 +1,28 @@
+import java.util.Scanner;
+
 public class start{
+    public static String get_dict(){
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+        scanner.close();
+        return result;
+    }
     public static void main(String[] args){
         //start up window
         //loop? class? function w/ return dict?
 
         //game setup
         String dict_name = get_dict();
-        read_dict(dict_name);
-        choose_random(dict_list);
-        int letter = calc_propabilities();
+        try {
+            dictionary dic = new dictionary(dict_name);
+        } catch (InvalidCountException | UndersizeException | UnbalncedException | InvalidRangeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //choose_random(dict_list);
+        //int letter = calc_propabilities();
 
         //game window
         //class
     }
-}
-
-
-class window{
-    create_window();
-    top_section();
-    middle_section();
-    bottom_section();
 }
